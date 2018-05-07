a6e3a7f6e3d99d642337dff4d18471c17a892f4b
David-Smith <david.smt@hotmail.com>
2018-05-07 18:06:45 -0300

    Start
    Start

---
+++ ProjetoLivrariaEstoque/Models/Contexto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ProjetoLivrariaEstoque.Models
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Livro> Livros { get; set; }
+
+        public Contexto(): base("BaseLivros")
+        {
+            Database.SetInitializer<Contexto>(new DropCreateDatabaseIfModelChanges<Contexto>());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ProjetoLivrariaEstoque/Models/Livro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoLivrariaEstoque.Models
+{
+    public class Livro
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public int Quantidade { get; set; }
+        public string Observacoes { get; set; }
+    }
+}
